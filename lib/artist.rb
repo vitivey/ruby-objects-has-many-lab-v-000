c6c56e101fb9c65959b2e8582901ec7ab8edad4a
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-15 14:24:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,6 +12,7 @@ class Artist
 
     def add_song(song)
       @songs << song
+      song.artist = @name #for the song instance, run artist method that is contained within Song class
     end
 
     def add_song_by_name
