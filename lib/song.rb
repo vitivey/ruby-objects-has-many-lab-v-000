84786bc2afc2bc71718f44c10bf81d4871382da9
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-15 14:30:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,7 @@ attr_accessor :name, :artist  #artist pulls in the artist instance from the Arti
         if @artist.name == nil
           nil
         else @artist.name  #@artist pulls in the Artist instance. From there, we call the name method from the Artist class
+        end
       end
 
 
