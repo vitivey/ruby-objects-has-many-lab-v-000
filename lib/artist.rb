9a1f5042ae3464f900480da613ea3da25155c50a
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-15 12:58:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,6 @@
 class Artist
-attr_accessor :name, :song
-@@song=[]
-
-    def initialize(name)
-      @name=name
-    end
-
-    def add_song(song)
-
-    end
-
-    def add_song_by_name(song)
-
-    end
-
-    def self.song_count
-
-    end
 
 
-    def songs
-      @@song
-    end
 
 
 end
