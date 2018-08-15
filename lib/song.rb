0549fe3d41e8ef28028d3ff47fb14352394eb993
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-15 14:25:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-attr_accessor :name
+attr_accessor :name, :artist
 @@all=[]
 
     def initialize(name)
@@ -11,9 +11,9 @@ attr_accessor :name
         @@all
       end
 
-      def artist
-        # @artist = Artist.new(artist)
-      end
+      # def artist
+      #   # @artist = Artist.new(artist)
+      # end
 
       def artist_name
         # @artist.name  #where @artist refers to the Artist Instance
